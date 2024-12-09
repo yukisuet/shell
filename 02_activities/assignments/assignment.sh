d32c60a0f7252763ec38c70dbc65dff314703eff
yukisuet <yukisze0215@gmail.com>
2024-12-09 10:49:33 -0500

    conflict resolved

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -35,7 +35,7 @@ cp ./data/raw/*server*.log ./data/processed/server_logs/
 cp ./data/raw/*user*.log ./data/processed/user_logs/
 cp ./data/raw/*event*.log ./data/processed/event_logs/
 # 7. For user privacy, remove all files containing IP addresses (files with "ipaddr" in the filename) from ./data/raw and ./data/processed/user_logs
-
+rf -rf ./data
 # 8. Create a file named ./data/inventory.txt that lists all the files in the subfolders of ./data/processed
 find ./data/processed -type f -exec basename {} \;> ./data/inventory.txt
 
